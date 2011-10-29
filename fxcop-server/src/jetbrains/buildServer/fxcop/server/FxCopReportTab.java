7fe719c0676cdf41807ffae5a15a5722e1c07ba8
Eugene.Petrenko <Eugene.Petrenko@jetbrains.com>
2011-10-29 18:04:38 +0000

    generify ViewLogTab and drop code duplication

--- fxcop-server/src/jetbrains/buildServer/fxcop/server/FxCopReportTab.java
+++ fxcop-server/src/jetbrains/buildServer/fxcop/server/FxCopReportTab.java
@@ -19,10 +19,10 @@ package jetbrains.buildServer.fxcop.server;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import jetbrains.buildServer.fxcop.common.FxCopConstants;
-import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
-import jetbrains.buildServer.serverSide.artifacts.BuildArtifactsViewMode;
 import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifactsViewMode;
 import jetbrains.buildServer.web.openapi.PagePlaces;
 import jetbrains.buildServer.web.openapi.ViewLogTab;
 import org.jetbrains.annotations.NotNull;
@@ -33,22 +33,23 @@ public class FxCopReportTab extends ViewLogTab {
   
   private static final String TAB_STARTPAGE = FxCopConstants.REPORT_FILE;
 
-  public FxCopReportTab(final PagePlaces pagePlaces, final SBuildServer server) {
+  public FxCopReportTab(@NotNull final PagePlaces pagePlaces,
+                        @NotNull final SBuildServer server) {
     super(TAB_TITLE, TAB_CODE, pagePlaces, server);
     setIncludeUrl("/artifactsViewer.jsp");
   }
 
   @Override
-  @SuppressWarnings({"unchecked"})
-  protected void fillModel(final Map model, final HttpServletRequest request, final SBuild build) {
+  protected void fillModel(@NotNull final Map<String, Object> model,
+                           @NotNull final HttpServletRequest request,
+                           @NotNull final SBuild build) {
     model.put("basePath", "");
     model.put("startPage", TAB_STARTPAGE);
   }
 
   @Override
-  public boolean isAvailable(@NotNull final HttpServletRequest request) {
-    final SBuild build = getBuild(request);
-    if (build == null) return false;
+  protected boolean isAvailable(@NotNull final HttpServletRequest request, @NotNull final SBuild build) {
+    if (!super.isAvailable(request, build)) return false;
 
     final BuildArtifact artifact = build.getArtifacts(BuildArtifactsViewMode.VIEW_ALL).getArtifact(TAB_STARTPAGE);
     return artifact != null && !artifact.isDirectory();
