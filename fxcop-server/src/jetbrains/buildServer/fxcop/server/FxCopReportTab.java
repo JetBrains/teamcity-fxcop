770abcc6aaddc5b8028f2d588377a9b96dcd3fa0
Maxim.Podkolzine <Maxim.Podkolzine@jetbrains.com>
2011-11-09 11:23:01 +0000

    TW-18903

--- fxcop-server/src/jetbrains/buildServer/fxcop/server/FxCopReportTab.java
+++ fxcop-server/src/jetbrains/buildServer/fxcop/server/FxCopReportTab.java
@@ -21,10 +21,9 @@ import javax.servlet.http.HttpServletRequest;
 import jetbrains.buildServer.fxcop.common.FxCopConstants;
 import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.serverSide.SBuildServer;
-import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
-import jetbrains.buildServer.serverSide.artifacts.BuildArtifactsViewMode;
 import jetbrains.buildServer.web.openapi.PagePlaces;
 import jetbrains.buildServer.web.openapi.ViewLogTab;
+import jetbrains.buildServer.web.reportTabs.ReportTabUtil;
 import org.jetbrains.annotations.NotNull;
 
 public class FxCopReportTab extends ViewLogTab {
@@ -33,25 +32,21 @@ public class FxCopReportTab extends ViewLogTab {
   
   private static final String TAB_STARTPAGE = FxCopConstants.REPORT_FILE;
 
-  public FxCopReportTab(@NotNull final PagePlaces pagePlaces,
-                        @NotNull final SBuildServer server) {
+  public FxCopReportTab(@NotNull PagePlaces pagePlaces,
+                        @NotNull SBuildServer server) {
     super(TAB_TITLE, TAB_CODE, pagePlaces, server);
     setIncludeUrl("/artifactsViewer.jsp");
   }
 
   @Override
-  protected void fillModel(@NotNull final Map<String, Object> model,
-                           @NotNull final HttpServletRequest request,
-                           @NotNull final SBuild build) {
-    model.put("basePath", "");
+  protected void fillModel(@NotNull Map<String, Object> model,
+                           @NotNull HttpServletRequest request,
+                           @NotNull SBuild build) {
     model.put("startPage", TAB_STARTPAGE);
   }
 
   @Override
-  protected boolean isAvailable(@NotNull final HttpServletRequest request, @NotNull final SBuild build) {
-    if (!super.isAvailable(request, build)) return false;
-
-    final BuildArtifact artifact = build.getArtifacts(BuildArtifactsViewMode.VIEW_ALL).getArtifact(TAB_STARTPAGE);
-    return artifact != null && !artifact.isDirectory();
+  protected boolean isAvailable(@NotNull HttpServletRequest request, @NotNull SBuild build) {
+    return super.isAvailable(request, build) && ReportTabUtil.isAvailable(build, TAB_STARTPAGE);
   }
 }
