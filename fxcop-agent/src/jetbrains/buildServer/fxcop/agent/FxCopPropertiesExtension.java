3cc1068bdc0fa498491f7b12ef48bf0ec2fa4f4a
Evgeniy.Koshkin <Evgeniy.Koshkin@jetbrains.com>
2012-01-11 16:53:26 +0000

    TW-12605 - detected FxCop 10.0 in VS2010 Premium installation

--- fxcop-agent/src/jetbrains/buildServer/fxcop/agent/FxCopPropertiesExtension.java
+++ fxcop-agent/src/jetbrains/buildServer/fxcop/agent/FxCopPropertiesExtension.java
@@ -17,6 +17,8 @@
 package jetbrains.buildServer.fxcop.agent;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 import jetbrains.buildServer.agent.AgentLifeCycleAdapter;
 import jetbrains.buildServer.agent.AgentLifeCycleListener;
 import jetbrains.buildServer.agent.BuildAgent;
@@ -27,12 +29,18 @@ import jetbrains.buildServer.util.EventDispatcher;
 import jetbrains.buildServer.util.PEReader.PEUtil;
 import jetbrains.buildServer.util.PEReader.PEVersion;
 import jetbrains.buildServer.util.Win32RegistryAccessor;
+import jetbrains.buildServer.util.positioning.PositionAware;
+import jetbrains.buildServer.util.positioning.PositionConstraint;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class FxCopPropertiesExtension extends AgentLifeCycleAdapter {
+public class FxCopPropertiesExtension extends AgentLifeCycleAdapter implements PositionAware {
+
   private static final Logger LOG = Logger.getLogger(FxCopPropertiesExtension.class);
+
+  private static final String VS_2010_PATH = "VS2010_Path";
+  public static final String FXCOP_EXE_RELATIVE_PATH = "..\\..\\Team Tools\\Static Analysis Tools\\FxCop\\FxCopCmd.exe";
 
   private final Win32RegistryAccessor myAccessor;
 
@@ -41,31 +49,61 @@ public class FxCopPropertiesExtension extends AgentLifeCycleAdapter {
     events.addListener(this);
   }
 
+  @NotNull
+  public String getOrderId() {
+    return FxCopConstants.RUNNER_TYPE;
+  }
+
+  @NotNull
+  public PositionConstraint getConstraint() {
+    return PositionConstraint.last();
+  }
+
   @Override
   public void beforeAgentConfigurationLoaded(@NotNull final BuildAgent agent) {
     final BuildAgentConfiguration config = agent.getConfiguration();
-    
     if (!config.getSystemInfo().isWindows()) {
       return;
     }
-
-    // Try to detect FxCop from windows registry
-    final String fxcopRoot = searchFxCopInClassesRoot();
-
-    if (fxcopRoot != null) {
-      final File fxcopCmd = new File(fxcopRoot, FxCopConstants.FXCOPCMD_BINARY);
-      PEVersion fileVersion = PEUtil.getFileVersion(fxcopCmd);
-      if (fileVersion != null) {
-        config.addCustomProperty(FxCopConstants.FXCOPCMD_FILE_VERSION_PROPERTY, fileVersion.toString());
-        LOG.info("Found FxCopCmd file version: " + fileVersion);
-      }
-
-      config.addCustomProperty(FxCopConstants.FXCOP_ROOT_PROPERTY, fxcopRoot);
+    final String fxcopRoot = searchFxCopInWinRegistry();
+    if(fxcopRoot == null) {
+      return;
     }
+    setupEnvironment(config, fxcopRoot);
+  }
+
+  @Override
+  public void agentInitialized(@NotNull final BuildAgent agent) {
+    final BuildAgentConfiguration config = agent.getConfiguration();
+    if (!config.getSystemInfo().isWindows() || config.getConfigurationParameters().containsKey(FxCopConstants.FXCOP_ROOT_PROPERTY)) {
+      return;
+    }
+    //TODO: introduce .net properties searcher in open api and use it here
+    final String fxcopRoot;
+    try {
+      fxcopRoot = searchFxCopInVS2010Installation(config);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return;
+    }
+    if (fxcopRoot == null) {
+      return;
+    }
+    setupEnvironment(config, fxcopRoot);
+  }
+
+  private void setupEnvironment(final BuildAgentConfiguration config, final String fxcopRoot) {
+    final File fxcopCmd = new File(fxcopRoot, FxCopConstants.FXCOPCMD_BINARY);
+    PEVersion fileVersion = PEUtil.getFileVersion(fxcopCmd);
+    if (fileVersion != null) {
+      config.addSystemProperty(FxCopConstants.FXCOPCMD_FILE_VERSION_PROPERTY, fileVersion.toString());
+      LOG.info("Found FxCopCmd file version: " + fileVersion);
+    }
+    config.addSystemProperty(FxCopConstants.FXCOP_ROOT_PROPERTY, fxcopRoot);
   }
 
   @Nullable
-  private String searchFxCopInClassesRoot() {
+  private String searchFxCopInWinRegistry() {
     // Use .fxcop file association
 
     final String fxcopClass =
@@ -116,5 +154,31 @@ public class FxCopPropertiesExtension extends AgentLifeCycleAdapter {
     }
 
     return bin;
+  }
+
+  @Nullable
+  private String searchFxCopInVS2010Installation(final BuildAgentConfiguration config) throws IOException {
+    final Map<String,String> configurationParameters = config.getConfigurationParameters();
+    if(!configurationParameters.containsKey(VS_2010_PATH)){
+      LOG.warn("VS2010_Path configuration parameter was not found");
+      return null;
+    }
+    final String vs2010Path =  configurationParameters.get(VS_2010_PATH);
+    if(vs2010Path == null || vs2010Path.isEmpty()) {
+      LOG.warn("VS2010_Path configuration parameter value is empty");
+      return null;
+    }
+    final File devenvExeHome = new File(vs2010Path);
+    if(!devenvExeHome.exists()){
+      LOG.warn("VS 2010 home directory was found in the agent configuration but non-existent on disk. Checked path: " + vs2010Path);
+      return null;
+    }
+    final File fxCopExe = new File(devenvExeHome, FXCOP_EXE_RELATIVE_PATH);
+    if(!fxCopExe.exists()){
+      LOG.warn("FxCopCmd.exe was not found in VS 2010 installation directory. Checked path: " + fxCopExe);
+      return null;
+    }
+    LOG.info("FxCopCmd.exe found on path " + fxCopExe);
+    return fxCopExe.getParentFile().getCanonicalPath();
   }
 }
