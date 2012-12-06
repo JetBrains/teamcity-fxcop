e15cff1c7c9a9b1fcbefc83dfa81c7c6b28272b3
Evgeniy.Koshkin <Evgeniy.Koshkin@jetbrains.com>
2012-12-06 12:48:38 +0000

    lost file

---
+++ fxcop-common/src/jetbrains/buildServer/fxcop/common/ArtifactsUtil.java
@@ -0,0 +1,15 @@
+package jetbrains.buildServer.fxcop.common;
+
+import java.lang.String;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Evgeniy.Koshkin
+ * Date: 06.12.12
+ * Time: 15:32
+ */
+public class ArtifactsUtil {
+  public static String getInternalArtifactPath(final String relativePath){
+    return String.format("%s/%s/%s", ".teamcity", FxCopConstants.RUNNER_TYPE, relativePath);
+  }
+}
