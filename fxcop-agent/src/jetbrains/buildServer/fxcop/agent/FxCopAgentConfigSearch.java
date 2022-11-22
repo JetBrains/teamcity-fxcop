65389d69c08646a0b79b0d3dc81404ad41f9a26c
Andre Rocha <andre.rocha@jetbrains.com>
2022-11-22 12:31:20 +0000

    TW-78410 execute dotnet parameter supplier lookup after ensuring all plugins have been registered

--- fxcop-agent/src/jetbrains/buildServer/fxcop/agent/FxCopAgentConfigSearch.java
+++ fxcop-agent/src/jetbrains/buildServer/fxcop/agent/FxCopAgentConfigSearch.java
@@ -20,9 +20,11 @@ import java.io.File;
 import java.util.Collection;
 import java.util.Collections;
 import jetbrains.buildServer.agent.BuildAgentConfiguration;
+import jetbrains.buildServer.agent.config.AgentParametersSupplier;
 import jetbrains.buildServer.fxcop.common.FxCopConstants;
 import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Provides fxCop info based on system parameter `FxCopRoot`.
@@ -30,7 +32,7 @@ import org.jetbrains.annotations.NotNull;
 public class FxCopAgentConfigSearch implements FxCopSearch {
   @NotNull
   @Override
-  public Collection<File> getHintPaths(@NotNull final BuildAgentConfiguration config) {
+  public Collection<File> getHintPaths(@NotNull final BuildAgentConfiguration config, @Nullable AgentParametersSupplier dotNetParametersSupplier) {
     final String fxCopRoot = config.getBuildParameters().getAllParameters().get(FxCopConstants.FXCOP_ROOT_PROPERTY);
     if (StringUtil.isEmpty(fxCopRoot)) {
       return Collections.emptyList();
--- fxcop-agent/src/jetbrains/buildServer/fxcop/agent/FxCopSearch.java
+++ fxcop-agent/src/jetbrains/buildServer/fxcop/agent/FxCopSearch.java
@@ -19,9 +19,11 @@ package jetbrains.buildServer.fxcop.agent;
 import java.io.File;
 import java.util.Collection;
 import jetbrains.buildServer.agent.BuildAgentConfiguration;
+import jetbrains.buildServer.agent.config.AgentParametersSupplier;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 interface FxCopSearch {
   @NotNull
-  Collection<File> getHintPaths(@NotNull final BuildAgentConfiguration config);
+  Collection<File> getHintPaths(@NotNull final BuildAgentConfiguration config, @Nullable AgentParametersSupplier dotNetParametersSupplier);
 }
